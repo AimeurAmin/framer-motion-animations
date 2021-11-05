e539d6827f1fa74ac219205f125d94cfedfa1992
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-11-05 03:41:42 +0100

    right and left animation

--- src/components/Box5.jsx
+++ src/components/Box5.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 
 function Box5() {
+  const control = useAnimation();
   return (
     <div className="box-container">
-      <button>Move Right</button>
-      <button>Move Left</button>
+      <button
+        onClick={() => {
+          control.start({
+            x: 1000,
+            transition: { duration: 1 }
+          })
+        }}
+      >Move Right</button>
+      <button
+        onClick={() => {
+          control.start({
+            x: 0,
+            transition: { duration: 1 }
+          })
+        }}
+      >Move Left</button>
       <button>Circle</button>
       <button>Square</button>
       <button>Stop</button>
-      <motion.div className="box"></motion.div>
+      <motion.div className="box"
+        animate={control}
+      ></motion.div>
     </div>
   )
 }
