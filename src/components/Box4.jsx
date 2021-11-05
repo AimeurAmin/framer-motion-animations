848db40cf8a1341696096d9a3baccde6be429017
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-11-05 03:16:42 +0100

    array of scales to use on animation

--- src/components/Box4.jsx
+++ src/components/Box4.jsx
@@ -4,7 +4,14 @@ import { motion } from 'framer-motion';
 function Box4() {
   return (
     <div className="box-container">
-      <motion.div className="box"></motion.div>
+      <motion.div className="box"
+        animate={{
+          scale: [1, .2, .5, .6, .7, .8, .9, 1]
+        }}
+        transition={{
+          duration: 2
+        }}
+      ></motion.div>
     </div>
   )
 }
