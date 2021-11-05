20a3ca8da708d85d2b1e66bcbb2e5413a428142a
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-11-05 02:28:12 +0100

    starter point for box2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Box1 from './components/Box1';
+import Box2 from './components/Box2';
 
 function App() {
   return (
     <div className="App">
+      <p>click the box</p>
       <Box1 />
+      <p>hover or drag the box</p>
+      <Box2 />
     </div>
   );
 }
