507ed9636f23ff085a60420bdc87e804f4baacd7
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-11-05 02:45:31 +0100

    adds box3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Box1 from './components/Box1';
 import Box2 from './components/Box2';
+import Box3 from './components/Box3';
 
 function App() {
   return (
     <div className="App">
       <p>click the box</p>
       <Box1 />
+
       <p>hover, click or drag the box</p>
       <Box2 />
+
+      <p>hover, click or drag the box</p>
+      <Box3 />
     </div>
   );
 }
---
+++ src/components/Box3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+function Box3() {
+  return (
+    <div className="box-container">
+      <motion.div className="box"></motion.div>
+    </div>
+  )
+}
+
+export default Box3
