f53ae07bacade922b3daaea115a4740d8ef8e9d6
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-11-05 03:16:20 +0100

    adds box4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Box1 from './components/Box1';
 import Box2 from './components/Box2';
 import Box3 from './components/Box3';
+import Box4 from './components/Box4';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
 
       <p>...</p>
       <Box3 />
+
+      <p>...</p>
+      <Box4 />
     </div>
   );
 }
